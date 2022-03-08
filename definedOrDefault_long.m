9e98a0f196c4fd093cda403cd747c7a71969ecc8
litost0803 <63686251+litost0803@users.noreply.github.com>
2022-03-08 04:02:18 -0700

    Add files via upload

---
+++ definedOrDefault_long.m
@@ -0,0 +1,8 @@
+function value = definedOrDefault_long(name,default,args_in)
+    ind = (find(strcmp(args_in,name)));
+    if isempty(ind)
+        value = default;
+    else
+        value = args_in{ind+1};
+    end
+end
